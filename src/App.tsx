51f0556ec90228b58f6743e2e80023508b603c33
bautidea <juanbautistadeangelis@gmail.com>
2024-05-25 11:32:47 -0300

    Creating custom hook to handle all reducer actions.

--- src/App.tsx
+++ src/App.tsx
@@ -1,72 +1,12 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
-import { useReducer } from 'react';
-import { State, Action } from './types.d';
-
-const initialState: State = {
-  fromLanguage: 'auto',
-  toLanguage: 'en',
-  fromText: '',
-  result: '',
-  loading: false,
-};
-
-function reducer(state: State, action: Action) {
-  const { type } = action;
-
-  switch (type) {
-    case 'SWITCH_LANGUAGE': {
-      return {
-        ...state,
-        fromLanguage: state.toLanguage,
-        toLanguage: state.fromLanguage,
-      };
-    };
-
-    case 'SET_FROM_LANGUAGE': {
-      return {
-        ...state,
-        fromLanguage: action.payload
-      };
-    };
-
-    case 'SET_TO_LANGUAGE' : {
-      return {
-        ...state,
-        toLanguage: action.payload
-      };
-    };
-
-    case 'SET_FROM_TEXT' : {
-      return {
-        ...state,
-        loading: true,
-        fromText: action.payload
-      };
-    };
-
-    case 'SET_RESULT' : {
-      return {
-        ...state,
-        loading: false,
-        result: action.payload
-      };
-    };
-  }
-
-  return state;
-}
+import useLanguages from './hooks/useLanguage';
 
 function App() {
-  const [state, dispatch] = useReducer(reducer, initialState);
+  const { fromLanguage, setFromLanguage } = useLanguages();
 
-  const { fromLanguage, toLanguage, fromText, result, loading } = state;
-
-  function handleClick () {
-    dispatch({
-      type : 'SET_FROM_LANGUAGE',
-      payload: 'espa'
-    })
+  function handleClick() {
+    setFromLanguage('ur');
   }
   return (
     <>
---
+++ src/hooks/useLanguage.ts
@@ -0,0 +1,110 @@
+import { useReducer } from 'react';
+import { State, Action } from '../types.d';
+
+const initialState: State = {
+  fromLanguage: 'auto',
+  toLanguage: 'en',
+  fromText: '',
+  result: '',
+  loading: false,
+};
+
+function reducer(state: State, action: Action) {
+  const { type } = action;
+
+  switch (type) {
+    case 'SWITCH_LANGUAGE': {
+      return {
+        ...state,
+        fromLanguage: state.toLanguage,
+        toLanguage: state.fromLanguage,
+      };
+    }
+
+    case 'SET_FROM_LANGUAGE': {
+      return {
+        ...state,
+        fromLanguage: action.payload,
+      };
+    }
+
+    case 'SET_TO_LANGUAGE': {
+      return {
+        ...state,
+        toLanguage: action.payload,
+      };
+    }
+
+    case 'SET_FROM_TEXT': {
+      return {
+        ...state,
+        loading: true,
+        fromText: action.payload,
+      };
+    }
+
+    case 'SET_RESULT': {
+      return {
+        ...state,
+        loading: false,
+        result: action.payload,
+      };
+    }
+  }
+
+  return state;
+}
+
+function useLanguages() {
+  const [state, dispatch] = useReducer(reducer, initialState);
+  const { fromLanguage, toLanguage, fromText, result, loading } = state;
+
+  function switchLanguages() {
+    dispatch({
+      type: 'SWITCH_LANGUAGE',
+    });
+  }
+
+  function setFromLanguage(newFromLanguage: string) {
+    dispatch({
+      type: 'SET_FROM_LANGUAGE',
+      payload: newFromLanguage,
+    });
+  }
+
+  function setToLanguage(newToLanguage: string) {
+    dispatch({
+      type: 'SET_TO_LANGUAGE',
+      payload: newToLanguage,
+    });
+  }
+
+  function setFromText(fromText: string) {
+    dispatch({
+      type: 'SET_FROM_TEXT',
+      payload: fromText,
+    });
+  }
+
+  function setResult(result: string) {
+    dispatch({
+      type: 'SET_RESULT',
+      payload: result,
+    });
+  }
+
+  return {
+    fromLanguage,
+    toLanguage,
+    fromText,
+    result,
+    loading,
+    switchLanguages,
+    setFromLanguage,
+    setToLanguage,
+    setFromText,
+    setResult,
+  };
+}
+
+export default useLanguages;
