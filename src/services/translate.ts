85a25fc67c22dcf1c7d4da66a6ada93aae6481d6
bautidea <juanbautistadeangelis@gmail.com>
2024-05-31 15:00:38 -0300

    Trying to fetch data from openai.

--- src/services/translate.ts
+++ src/services/translate.ts
@@ -1,3 +1,6 @@
-import { configuration, OpenAIApi} from "openai"
-
+import Configuration from 'openai'
+import {OpenAIApi} from 'openai'
 const apiKey = import.meta.env.VITE_OPENAI_API_KEY
+
+const config = new Configuration({ apiKey })
+const openai = new OpenAIApi(config)
